0a4bc044d3370be33427783e9a0a27eca4efa2f8
Galassios <132308621+Galassios@users.noreply.github.com>
2026-01-16 21:30:46 -0300

    Add SQL script for salary analysis with Pandas

---
+++ projeto-02-analise-salarial-join/sql/analise_salarial_join.sql
@@ -0,0 +1,1 @@
+import pandas as pd df = pd.read_csv('dados/dados_pessoas.csv') resultado = ( df .drop_duplicates() .groupby('area') .agg( media=('salario', 'mean'), quant=('nome', 'count') ) .round(2) .sort_values(by=['media', 'quant'], ascending=[False, False]) ) print(resultado)
